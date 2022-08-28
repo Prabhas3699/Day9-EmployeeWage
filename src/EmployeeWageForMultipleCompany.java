5b155c6c5062cbb13e1c51f0cad33692b9529543
Prabhas3699 <prabhak620@gmail.com>
2022-08-28 10:45:56 +0530

    Refactor employee Wage multiple company

---
+++ src/EmployeeWageForMultipleCompany.java
@@ -0,0 +1,9 @@
+
+public class EmployeeWageForMultipleCompany extends RefactorEmployeeWageUC8{
+	/*
+	 * inherting the RefactorEmployeeWage
+	 */
+	public static void main(String []args) {
+		RefactorEmployeeWage("Google", 25, 100, 20);		
+	}
+}
