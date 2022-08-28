e376bb72a97faaed98eb132317777131b93631eb
Prabhas3699 <prabhak620@gmail.com>
2022-08-28 10:43:10 +0530

    Refactor employee Wage

---
+++ src/EmployeeWageUC7.java
@@ -0,0 +1,13 @@
+
+public class EmployeeWageUC7 extends RefactorEmployeeWageUC7{
+	/*
+	 * inherting the RefactorEmployeeWage
+	 */
+	public static void main(String[] args) {
+		/*
+		 * calling method for RefactorEmployeeWage
+		 */
+		RefactorEmployeeWage();		
+	}
+
+}
